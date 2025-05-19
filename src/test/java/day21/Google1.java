babc033afbaaa4905ae216997fc018d68765737d
veerapandims <veerapandims0601@gmail.com>
2025-05-19 13:56:13 +0530

    task1

---
+++ src/test/java/day21/Google1.java
@@ -0,0 +1,35 @@
+package day21;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Google1 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.manage().window().maximize();
+
+            driver.get("http://google.com");
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            driver.navigate().refresh();
+
+            Thread.sleep(4000);
+        }
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+        finally 
+        {
+            driver.quit();
+        }
+    }
+
+}
+
+
